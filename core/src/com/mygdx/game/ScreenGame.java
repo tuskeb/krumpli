89e1ecf71674338f6f6ad0193fa662c10269a76f
0x686f7279 <nemtudokolvasni@gmail.com>
2015-11-20 20:02:10 +0100

    Egy árva hiba.

--- core/src/com/mygdx/game/ScreenGame.java
+++ core/src/com/mygdx/game/ScreenGame.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
@@ -53,7 +55,57 @@ Box2DDebugRenderer debugRenderer = new Box2DDebugRenderer();
 		super.show();
 
 		// átadjuk neki a vezérlést
+		Gdx.input.setInputProcessor(new InputProcessor() {
+			@Override
+			public boolean keyDown(int keycode) {
+				switch (keycode) {
+					case Input.Keys.ESCAPE:
+					case Input.Keys.BACK:
 
+						break;
+					case Input.Keys.SPACE:
+						spaceShip.setRocketState(ActorSpaceship.RocketType.landing, true);
+						break;
+				}
+
+				return false;
+			}
+
+			@Override
+			public boolean keyUp(int keycode) {
+				return false;
+			}
+
+			@Override
+			public boolean keyTyped(char character) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDragged(int screenX, int screenY, int pointer) {
+				return false;
+			}
+
+			@Override
+			public boolean mouseMoved(int screenX, int screenY) {
+				return false;
+			}
+
+			@Override
+			public boolean scrolled(int amount) {
+				return false;
+			}
+		});
 	}
 
 
@@ -78,18 +130,4 @@ Box2DDebugRenderer debugRenderer = new Box2DDebugRenderer();
 
 	}
 
-	@Override
-	public boolean keyDown(int keycode) {
-		switch (keycode) {
-			case Input.Keys.ESCAPE:
-			case Input.Keys.BACK:
-
-				break;
-            case Input.Keys.SPACE:
-                spaceShip.setRocketState(ActorSpaceship.RocketType.landing, true);
-                break;
-		}
-
-		return false;
-	}
 }
